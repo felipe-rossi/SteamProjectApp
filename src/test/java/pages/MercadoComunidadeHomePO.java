d45cad57ff4da8e3ddb7ee461695b5abdba22bb2
Felipe Rossi <91573168+felipe-rossi@users.noreply.github.com>
2023-06-19 15:09:37 -0300

    Adicionando um Wait visibility no filtro de facas2

--- src/test/java/pages/MercadoComunidadeHomePO.java
+++ src/test/java/pages/MercadoComunidadeHomePO.java
@@ -34,7 +34,7 @@ public class MercadoComunidadeHomePO extends BasePage {
     public ResultadoMercadoPO pesquisarPorFacas(){
        driver.findElement(By.xpath("//*[@resource-id='market_search_advanced_show']")).click();
        WebElement checkFacas = driver.findElement(MobileBy.androidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().resourceId(\"market_advancedsearch_filters\").className(\"android.view.View\").className(\"android.widget.CheckBox\").resourceId(\"tag_730_Type_CSGO_Type_Knife\"))")); //tag_730_Type_CSGO_Type_Rifle  tag_730_Type_CSGO_Type_Knife
-       wait.until(ExpectedConditions.visibilityOf(checkFacas)).click();
+       checkFacas.click();
        metodos.scroll(1);
        WebElement btnBuscar = driver.findElement(By.xpath("//*[@text='Buscar']"));
        btnBuscar.click();
